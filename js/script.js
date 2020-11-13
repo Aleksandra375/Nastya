311791de83e96dc5b45670ba304a4912a995e1b8
Aleksandra375 <mykhailova.alexandra11@gmail.com>
2020-11-13 13:10:31 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        nextArrow: '<button class="slider-controls slider-next">&gt;</button>',
+        prevArrow: '<button class="slider-controls slider-prev">&lt;</button>',
+        dots: true,
+        dotsClass: 'slider-dots',
+    });
+});
+
+
